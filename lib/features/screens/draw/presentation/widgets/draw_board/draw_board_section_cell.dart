a8a5eeea1a0aa79723cec2c4c07b1591e538b256
Rui Cao <rayray199085@gmail.com>
2025-01-18 15:41:44 +1100

    Update board section cell animation

--- lib/features/screens/draw/presentation/widgets/draw_board/draw_board_section_cell.dart
+++ lib/features/screens/draw/presentation/widgets/draw_board/draw_board_section_cell.dart
@@ -24,38 +24,42 @@ class DrawBoardSectionCell extends StatefulWidget {
 
 class _DrawBoardSectionCellState extends State<DrawBoardSectionCell>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _sizeAnimation;
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
   bool _isAnimating = false;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = AnimationController(
+    _animationController = AnimationController(
       vsync: this,
       duration: const Duration(
           milliseconds:
               _DrawBoardSectionCellConstants.animationDurationInMilliseconds),
     );
 
-    _sizeAnimation = Tween<double>(
-            begin: _DrawBoardSectionCellConstants.scaleFactorFrom,
-            end: _DrawBoardSectionCellConstants.scaleFactorTo)
-        .animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
-    );
-
-    _controller.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        _controller.reverse(); // Shrink back to normal size
-      }
-    });
+    _scaleAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween<double>(
+          begin: _DrawBoardSectionCellConstants.scaleFactorFrom,
+          end: _DrawBoardSectionCellConstants.scaleFactorTo,
+        ).chain(CurveTween(curve: Curves.easeIn)),
+        weight: 1.0, // Duration proportion for this animation
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(
+          begin: _DrawBoardSectionCellConstants.scaleFactorTo,
+          end: _DrawBoardSectionCellConstants.scaleFactorFrom,
+        ).chain(CurveTween(curve: Curves.easeOut)),
+        weight: 1.0,
+      ),
+    ]).animate(_animationController);
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -71,39 +75,44 @@ class _DrawBoardSectionCellState extends State<DrawBoardSectionCell>
         final hasSelected = numbers.contains(widget.number);
         if (shouldHighlight && !_isAnimating) {
           _isAnimating = true;
-          _controller.forward().then((_) => _isAnimating = false);
+          _animationController.forward().then((_) => _isAnimating = false);
         }
 
-        return AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Transform.scale(
-              scale: shouldHighlight
-                  ? _sizeAnimation.value
-                  : _DrawBoardSectionCellConstants.scaleFactorFrom,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: hasSelected
-                      ? Theme.of(context).colorScheme.tertiary
-                      : Colors.black26,
-                  borderRadius: BorderRadius.circular(RadiusValues.circular4),
+        return OverflowBox(
+          maxHeight: double.infinity,
+          maxWidth: double.infinity,
+          child: AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, child) {
+              return Transform.scale(
+                scale: shouldHighlight
+                    ? _scaleAnimation.value
+                    : _DrawBoardSectionCellConstants.scaleFactorFrom,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: hasSelected
+                        ? Theme.of(context).colorScheme.tertiary
+                        : Colors.black26,
+                    borderRadius: BorderRadius.circular(RadiusValues.circular4),
+                  ),
+                  alignment: Alignment.center,
+                  child: hasSelected
+                      ? Text(
+                          widget.number.toString(),
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium
+                              ?.copyWith(
+                                color:
+                                    Theme.of(context).colorScheme.onSecondary,
+                                fontWeight: FontWeight.bold,
+                              ),
+                        )
+                      : const SizedBox.shrink(),
                 ),
-                alignment: Alignment.center,
-                child: hasSelected
-                    ? Text(
-                        widget.number.toString(),
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelMedium
-                            ?.copyWith(
-                              color: Theme.of(context).colorScheme.onSecondary,
-                              fontWeight: FontWeight.bold,
-                            ),
-                      )
-                    : const SizedBox.shrink(),
-              ),
-            );
-          },
+              );
+            },
+          ),
         );
       },
     );
